46fd3f7c687538c97b43603f9e7a5b681ca6a7dd
Martin Parker <martinparker68@gmail.com>
2025-02-13 23:16:08 +0000

    Add files via upload

--- lucas.c
+++ lucas.c
@@ -38,7 +38,49 @@ int ticks_on[16];
 int ticks_off[16];
 int phase[16];
 
-void lamps_thread()
+void no_phasor()
+{
+    uint64_t wanted;
+    int tick[16];
+    int ton[16];
+    int toff[16];
+    int x,count;
+
+    for (x=0;x<16;x++)
+    {	    
+    	tick[x]=0; 
+	ton[x]=0;
+	toff[x]=0;
+    }
+    wanted=time_us_64();
+    while (1)
+    {
+   	wanted+=DELAY;
+	count=0;
+	while (time_us_64()<wanted)
+	{
+		sleep_us(1);
+		//count++;
+	}
+	//if (count<1){ gpio_put(PICO_DEFAULT_LED_PIN,1);}
+	for (x=0;x<16;x++)
+	{
+		if(ticks_on[x]==0){tick[x]=0;continue;}
+		if (tick[x]==ton[x]){ 
+			gpio_put(D0+x,0);
+			toff[x]=ticks_off[x];
+			ton[x]=ticks_on[x];
+			tick[x]=ton[x];
+		}else if (tick[x]>=toff[x]){
+			gpio_put(D0+x,1);
+		}
+		tick[x]++;
+		if (tick[x]>toff[x]){ tick[x]=0;}
+	}
+    }
+}
+
+void phasor()
 {
     uint64_t wanted;
     int tick[16];
@@ -84,7 +126,7 @@ void lamps_thread()
 		} 
 
 		tick[x]++;
-		if (tick[x]>toff[x]){ tick[x]=0;}
+		if (tick[x]>toff[x] || toff[x]==0){ tick[x]=0;}
 	}
     }
 }
@@ -136,18 +178,14 @@ int init_pins ()
 
 int main()
 {
-	// adding stdio causes interrupts and slows everything down. Debug only.
-    	//stdio_init_all();
 	init_pins();
 
-	sleep_ms(100);
-	
+	sleep_ms(100); // wait a little for the gpio
+
+	float ncent,mcent,nfact,dec; 
 	int x,attack,press,release;
 	float a,cent,fact;
-	cent=140;
-	fact=0;
-	attack=0;
-	release=1;
+	cent=140;dec=1; fact=0; attack=0; release=1; a=1;
 
 	for (x=0;x<16;x++)
 	{
@@ -156,52 +194,174 @@ int main()
 		phase[x]=0;
 	}
 
-	a=1;
-    	multicore_launch_core1(lamps_thread);
-	float ncent;
-	float mcent;
-	float nfact;
-	float dec;
 
-	dec=1;
-
-	while (1)
-	{
-		//gpio_put(PICO_DEFAULT_LED_PIN,0);
-		sleep_us(30);
-
+	press=gpio_get(I0);  //pressed button on power changes the mode
+	if ( press) { 
+    		multicore_launch_core1(no_phasor);
     		adc_select_input(0);
-       		ncent = ((float)adc_read());
-		cent=((1000*cent)+ncent)/1001;
-    		adc_select_input(1);
-       		nfact = (float)adc_read()/2048;
-		//if (nfact<0.005){nfact=0;}
-		fact=((1000*fact)+nfact)/1001;
-		mcent=40+((cent*cent)/4000);
-
-		press=gpio_get(I0); 
-		if ( press){ 
-			if (release){ release=0; dec=1;}
-			if (! attack){
-			attack=1; 
-			a=0.0001;}
-			dec*=0.9995;
-			if (dec<0.001){dec=0.001;}
-		}else{
-			if (attack){ release=1;}
-		}
-
-		if (attack==1){ a*=1.005; if(a>=1){a=1;if(!press){attack=0;}}}
-
-		for (x=0;x<16;x+=2)
+       		if (adc_read()<2048)
 		{
-			float f,df;
-			df=(((float)(7-x))*fact/8);
-			f=mcent+((df*mcent));
-			ticks_on[x]=1000000/(2*f*DELAY);
-			ticks_off[x]=ticks_on[x]*2;
-			phase[x]=((1-a)*(float)ticks_on[x]);
+			// random frequencies at random times 
+			while (1)
+			{
+				float freq[16];
+				sleep_us(30);
+
+    				adc_select_input(0);
+       				ncent = ((float)adc_read())/2000;
+				cent=((1000*cent)+ncent)/1001;
+    				adc_select_input(1);
+       				nfact = (float)adc_read();
+				fact=((1000*fact)+nfact)/1001;
+				mcent=30+((cent*cent)/4000);
+
+				press=gpio_get(I0); 
+
+				for (x=0;x<16;x++)
+				{		
+					float f,df;
+					df=(((float)(7-x))*fact/8);
+					f=mcent+((df*mcent));
+					if (rand()%10000000<(int)nfact){press=0;}
+					if ( !press){ 
+					ticks_on[x]=0;
+					freq[x]=pow((float)(rand()%10000)/100,2)+100;
+					}else{
+					ticks_on[x]=1000000/(cent*2*freq[x]*DELAY);
+					ticks_off[x]=ticks_on[x]*2;
+					}
+				}	
+			}
+		}else{
+			// fairy time 
+			float counter;
+			counter=0.99;
+			float freq[17];
+			int del;
+			del=0;
+			while (1)
+			{
+				sleep_us(30);
+
+    				adc_select_input(0);
+       				ncent = ((float)adc_read());
+				cent=((1000*cent)+ncent)/1001;
+    				adc_select_input(1);
+       				nfact = (float)adc_read()/4096;
+				fact=((1000*fact)+nfact)/1001;
+				mcent=30+((cent*cent)/4000);
+
+
+				press=gpio_get(I0); 
+
+				if (press){ counter*=(1.00001+(8*nfact/10000));if (counter>16){counter=16;}}else{counter*=(0.99999-(8*nfact/10000)); if (counter<0.99){counter=0.99;}}
+				freq[15]=cent;
+
+				for (x=0;x<16;x++)
+				{
+					if ( x<(int)counter){ 
+						ticks_on[x]=1000000/(2*freq[x]*DELAY);
+						ticks_off[x]=ticks_on[x]*2;
+					}else{
+						ticks_on[x]=0;}
+					freq[x-1]=freq[x]*0.96;
+				}	
+			}
 		}
-		if(!attack){if (a>0 ){a-=dec/100;}else{a=0;dec=1;}}
+	// the original phased amplitude
+	} else{
+    		adc_select_input(0);
+       		if (adc_read()<2048)
+		{
+    			multicore_launch_core1(phasor);
+			while (1)
+			{
+				sleep_us(30);
+    				adc_select_input(0);
+       				ncent = ((float)adc_read());
+				cent=((1000*cent)+ncent)/1001;
+    				adc_select_input(1);
+       				nfact = (float)adc_read()/2048;
+				fact=((1000*fact)+nfact)/1001;
+				mcent=30+((cent*cent)/4000);
+
+				press=gpio_get(I0); 
+				if ( press){ 
+					if (release){ release=0; dec=1;}
+					if (! attack){
+						attack=1; 
+						a=0.0001;}
+						dec*=0.9995;
+					if (dec<0.001){dec=0.001;}
+				}else{
+					if (attack){ release=1;}
+				}
+
+				if (attack==1){ a*=1.005; if(a>=1){a=1;if(!press){attack=0;}}}
+
+				for (x=0;x<16;x+=2)
+				{
+					float f,df;
+					df=(((float)(7-x))*fact/8);
+					f=mcent+((df*mcent));
+					ticks_on[x]=1000000/(2*f*DELAY);
+					ticks_off[x]=ticks_on[x]*2;
+					phase[x]=((1-a)*(float)ticks_on[x]);
+				}	
+				if(!attack){if (a>0 ){a-=dec/100;}else{a=0;dec=1;}}
+			}
+
+		}else {
+    			multicore_launch_core1(no_phasor);
+			float freq[17];
+			float p;
+			p=pow(2,0.08333333);
+			int count,pressing,rr,rt,fc;
+			count=-1;
+			pressing=0;
+			rr=0;
+			rt=10;
+			fc=0;
+					for (x=0;x<16;x++){ freq[x]=100;}
+			while (1)
+			{
+				sleep_us(30);
+    				adc_select_input(0);
+       				ncent = ((float)adc_read());
+				cent=((1000*cent)+ncent)/1001;
+    				adc_select_input(1);
+       				nfact = (float)adc_read();
+				fact=((1000*fact)+nfact)/1001;
+				mcent=30+((cent*cent)/4000);
+
+
+				//if (rr==rt)
+				//{
+					//rt=20+rand()%(((int)fact+1)*1000);
+					fc++;if (fc>15){fc=0;}
+					freq[fc]+=freq[fc]*fact*(0.5-((float)(rand()%10000)/10000))/400000;
+				//}
+
+				press=gpio_get(I0); 
+				if ( press )
+				{
+					if (! pressing)
+					{
+						count++; if (count>3){ count=0;}
+					}
+					for (x=4*count;x<4*(count+1);x++){ freq[x]=mcent;}
+					pressing=10000;
+				}else{ pressing--; if (pressing<0){pressing=0;}}
+
+				for (x=0;x<16;x++)
+				{
+					ticks_on[x]=1000000/(2*freq[x]*DELAY);
+					ticks_off[x]=ticks_on[x]*2;
+				}	
+
+				rr++;
+			}
+
+		}
 	}
 }
